8bc1c36b876dc2b2167219657d5dd26c88b4d70a
Romain Bourdain <romain.bourdain93@gmail.com>
2024-03-26 15:13:09 +0100

    configuring clang format and updating gitignore

--- include/window.h
+++ include/window.h
@@ -3,17 +3,16 @@
 #include <SDL.h>
 #include <iostream>
 
-class Window
-{
-public:
+class Window {
+ public:
   Window();
   ~Window();
   void init() const;
   void run() const;
   void render() const;
-  void handle_events(const SDL_Event &event) const;
+  void handle_events(const SDL_Event& event) const;
 
-private:
+ private:
   std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> m_window;
   std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> m_renderer;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <SDL.h>
-#include <iostream>
 #include <cassert>
+#include <iostream>
 
 #include "const.h"
 #include "window.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
   Window window;
 
   window.run();
